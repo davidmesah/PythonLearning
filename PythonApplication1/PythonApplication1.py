dc72971fec8d6df1d224e0dd09f1faec537cdc17
Mesa Ortiz <Mesa Ortiz@DESKTOP-F6SS4NS>
2022-12-28 15:07:26 -0500

    Ejercicio 3

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,9 @@
+
+#Indice de Masa corporal
+
+p = input("Ingrese su Peso en Kg: ")
+e = input("Ingrese su Estatura en M: ")
+m = float(p)/float(e)
+r = "Su masa corporal es: " + str(round(m, 2))
+
+print(r) 
