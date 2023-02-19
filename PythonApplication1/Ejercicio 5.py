b711d95c4c6ddaa97c8109f6c11f09a082909039
Mesa Ortiz <Mesa Ortiz@DESKTOP-F6SS4NS>
2023-02-19 11:54:46 -0500

    Ejercicio 5

---
+++ PythonApplication1/Ejercicio 5.py
@@ -0,0 +1,11 @@
+
+def es_bisiesto(t):
+    return t % 400 == 0 or (t % 100 != 0 and t % 4 == 0)
+
+print("Comprobador de años bisiestos")
+fecha = int(input("Escriba un año y le diré si es bisiesto: "))
+if es_bisiesto(fecha):
+    print("El año", fecha, "es un año bisiesto.")
+else:
+    print("El año", fecha, "no es un año bisiesto.")
+
