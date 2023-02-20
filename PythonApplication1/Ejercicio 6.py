9f3b4b431b0c760e16db8f0be98d5ba70f5b6f8d
Mesa Ortiz <Mesa Ortiz@DESKTOP-F6SS4NS>
2023-02-19 21:09:00 -0500

    Finalizado

--- PythonApplication1/Ejercicio 6.py
+++ PythonApplication1/Ejercicio 6.py
@@ -4,7 +4,14 @@
 
 def fizzbuzz():
     for index in range(1, 101):
-        print(index)
+        if index % 3 == 0 and index % 5 == 0:
+            print("fizzbuzz")
+        elif index % 3 == 0:
+            print("fizz")
+        elif index % 5 == 0:
+            print("buzz")
+        else:
+            print(index)
 
 fizzbuzz()
 
