b4192c599627e68f0eb84df3f68578192a4b4b47
Mesa Ortiz <Mesa Ortiz@DESKTOP-F6SS4NS>
2023-02-19 21:25:32 -0500

    Resolución de Ejercicio

--- PythonApplication1/Ejercicio 7.py
+++ PythonApplication1/Ejercicio 7.py
@@ -4,6 +4,6 @@
 def is_anagram(word_one, word_two):
     if word_one.lower() == word_two.lower():
         return False
-    return word_one == word_two
+    return sorted(word_one.lower()) == sorted(word_two.lower())
 
 print(is_anagram("Amor", "Roma"))
