436519f3a4a66f80023a5b1138375d0e1e124070
Mesa Ortiz <Mesa Ortiz@DESKTOP-F6SS4NS>
2023-02-20 10:07:36 -0500

    Ejercicio NO. 9

---
+++ PythonApplication1/Ejercicio 9.py
@@ -0,0 +1,22 @@
+#¿ES UN NÚMERO PRIMO?
+
+# Escribe un programa que se encargue de comprobar si un número es o no primo.
+# Hecho esto, imprime los números primos entre 1 y 100.
+
+def num_primo():
+
+    for number in range(1, 101):
+
+        if number > 1:
+
+            is_divisible = False
+
+            for i in range(2, number):
+              if number % i == 0:
+                  is_divisible = True
+                  break 
+
+            if not is_divisible:
+                print(number)
+
+num_primo()
